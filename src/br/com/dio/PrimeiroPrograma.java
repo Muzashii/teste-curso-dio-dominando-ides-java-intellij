82c4f5e37a0b59d4a7a281ad2903723d8067dc73
Muzashii <evertoncosta100@gmail.com>
2022-01-11 13:35:35 -0300

    fist commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,18 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+
+        Gato gato = new Gato("jorge", "amarelo", 18);
+
+        System.out.println(gato);
+
+
+/*      int a =5;
+        int b =3;
+
+        System.out.println("Hello Word! " + (a+b));*/
+    }
+}
